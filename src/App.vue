b0a4807e7d40993f0fe4c82d1b36d11f449a000b
zeyap <zeyap@qq.com>
2018-08-28 20:57:36 -0400

    homework1 - init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/photo.jpg">
-    <h1>Hi! I'm Shea Belsky!</h1>
-    <h2>I'm a Head TA for INFO/CS 4340 this semester.</h2>
-    <p>I was a UX Engineer at Google this summer, working on prototypes for Google Search and Assistant.</p>
-    <p>I love to run, ski, read, and nosh on pierogees.</p>
+    <div id = "selfIntro">
+    <h2>Hi! I'm Zeya Peng!</h2>
+    <h3>I'm a first-year in MPS IS.</h3>
+    <p>_(:3JL)_</p>
+    <p>I am a psychology major undergrad before this semester, where I got very interested in Human Computer Interaction and decided to learn more in the tech side in HCI.
+      Over a year later I got into this program and interned as a front end developer in a mini company in my place (somehow).
+    </p>
+    <p>It is so excited to be part of the studio to make something cool.</p>
+    <p>(Besides I hope to be just as magnificent as our TA to work for Google.)</p>
+    <p>I am a sedentary kind of person and I am once a frequent movie-goer/ TV shows watcher, not-so-hardcore game player (and developer for a few times at my local Game Jam). </p>
+    </div>
     <h2>Chuck Norris Jokes</h2>
     <div class="chuckNorris">
       <div class="field">
@@ -56,8 +62,11 @@ export default class App extends Vue {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
+  color: #494945;
+  background: #c6ffdd; /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #c6ffdd, #fbd786, #f7797d); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #c6ffdd, #fbd786, #f7797d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -71,5 +80,9 @@ img {
 }
 input {
   margin-left: 5px;
+}
+#selfIntro{
+  margin: 0 auto;
+  width: 50%;
 }
 </style>
